85e0c52c94c6ec745a776943e4c4150456890922
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2024-03-06 19:11:34 -0700

    Get knightMoves function working

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function levelOrder(start) {
   startNode.visited = true;
   while (queue[0]) {
     let currentNode = queue[0];
-    results.push(queue.shift().square);
+    results.push(queue.shift());
     if (currentNode.adjacencies) {
       for (let adjacency of currentNode.adjacencies) {
         if (getNode(adjacency).visited === false) {
@@ -97,25 +97,46 @@ function levelOrder(start) {
   return results;
 }
 
-// I know that I am only one move away from finishing when the current node and end node
-// share an adjacency.
-// I can take my level order array and iterate until a node shares an adjacency with my end node.
-// Then, I can take that adjacency and find an adjacency to that adjacency that has a lower index. Then
+const findLowerIndexAdjacency = function (levelOrderArray, endIndex) {
+  for (let adjacency of levelOrderArray[endIndex].adjacencies) {
+    if (levelOrderArray.indexOf(getNode(adjacency)) < endIndex) {
+      return levelOrderArray.indexOf(getNode(adjacency));
+    }
+  }
+};
+// I can take my level order array and iterate until I reach the end node.
+// Then, I can take that node and find an adjacency to that node that has a lower index. Then
 // I can repeat the process until I reach the start node. This works because I know that any adjacency
 // with a lower index than the current node must be "closer" to the start node, due to the fact that
 // the array is in level-order
 function knightMoves(start, end) {
-  let startNode = getNode(start);
+  let pathArray = [end];
   let endNode = getNode(end);
   let levelOrderArray = levelOrder(start);
+  let endIndex;
+  for (let i = 0; i < levelOrderArray.length; i++) {
+    if (levelOrderArray[i] === endNode) {
+      endIndex = i;
+      break;
+    }
+  }
+  while (endIndex !== 0) {
+    endIndex = findLowerIndexAdjacency(levelOrderArray, endIndex);
+    pathArray.unshift(levelOrderArray[endIndex].square);
+  }
+  return pathArray;
 }
 
+for (let item of knightMoves([0, 0], [7, 7])) {
+  console.log(item);
+}
 //console.log(knightMoves([0, 0], [7, 7]));
-//console.log(getNode([0, 0]));
 
+/*
 for (let item of levelOrder([0, 0])) {
   console.log(item);
 }
 
 console.log(board1.boardArray.length);
 console.log(levelOrder([0, 0]).length);
+*/
